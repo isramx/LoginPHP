99d238ef1eed0fae2498c0ea2fe818af3a89e70f
Angel Omar Reyes Santos <22589@virtual.utsc.edu.mx>
2026-07-26 15:06:21 -0600

    Activar conexion SSL MySQL nube

--- configuracion.php
+++ configuracion.php
@@ -4,7 +4,19 @@ $user = "c7fe7d25f5a22a90";
 $pass = "821c212ad130366ea9c1b35dfdc60c";
 $bd = "cd6542194613";
 
-$conn = new mysqli($server, $user, $pass, $bd);
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+$conn->real_connect(
+    $server,
+    $user,
+    $pass,
+    $bd,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
