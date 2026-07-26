db1116d9c560e2622ba1fb8d2303e1a02a5cb768
Angel Omar Reyes Santos <22589@virtual.utsc.edu.mx>
2026-07-26 15:10:30 -0600

    Configurar SSL en conexion PHP

--- conexion.php
+++ conexion.php
@@ -1,10 +1,26 @@
 <?php
-include("configuracion.php");
-$conexion = new mysqli($server,$user,$pass,$bd); 
-if (mysqli_connect_errno()) {
-	echo "No Conectado", mysqli_connect_error(); 
-	exit();
-}//else{
-//	echo 'Conectado';
-//}
+
+$server = "primary.mysql--lkg5gb9wtp9l.addon.code.run";
+$user = "c7fe7d25f5a22a90";
+$pass = "821c212ad130366ea9c1b35dfdc60c";
+$bd = "cd6542194613";
+
+$conexion = mysqli_init();
+
+mysqli_ssl_set($conexion, NULL, NULL, NULL, NULL, NULL);
+
+$conexion->real_connect(
+    $server,
+    $user,
+    $pass,
+    $bd,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
 ?>
